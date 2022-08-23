77ff916fa71e87a6c1e244586462c7cfd0157bdf
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-08-23 14:41:42 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import { getImageTensorFromPath } from './imageHelper';
+// import { getImageTensorFromPath } from './imageHelper';
 const ort = require('onnxruntime-web');
 
 // use an async context to call onnxruntime functions.
