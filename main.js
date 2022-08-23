60e2d3608517eb2d87ae32871495aa29051bb674
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-08-23 16:16:07 -0700

    Update main.js

--- main.js
+++ main.js
@@ -13,42 +13,37 @@ async function main() {
     var image = document.getElementById('image');
 
     var div = document.createElement("DIV");
-    div.id = 'image_text';
+    div.id = 'output_text';
     div.innerHTML = `Image Source: ${image.src}`;
     document.body.appendChild(div);
 
-    var out_div = document.createElement("DIV");
-    out_div.id = 'output_text';
-    document.body.appendChild(out_div);
-
     try {
-        // window.alert("Hello World!");
-        // create a new session and load the specific model.
-        //
-        // the model in this example contains a single MatMul node
-        // it has 2 inputs: 'a'(float32, 3x4) and 'b'(float32, 4x3)
-        // it has 1 output: 'c'(float32, 3x3)
+        // create a new session and load the specified model.
         session = await ort.InferenceSession.create('squeezenet1_1.onnx',
             { executionProviders: ['wasm'], graphOptimizationLevel: 'all' });
 
-        // // prepare inputs. a tensor need its corresponding TypedArray as data
-        // const dataA = Float32Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
-        // const dataB = Float32Array.from([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]);
-        // const tensorA = new ort.Tensor('float32', dataA, [3, 4]);
-        // const tensorB = new ort.Tensor('float32', dataB, [4, 3]);
 
-        // // prepare feeds. use model input names as keys.
+        document.getElementById('output_text').innerHTML += 'Created inference session.';
+    } catch (e) {
+        document.getElementById('output_text').innerHTML += `failed to create inference session: ${e}.`;
+    }
+
+    try {
+        // prepare inputs. a tensor need its corresponding TypedArray as data
+        // const dataA = Float32Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+        // const input_tensor = new ort.Tensor('float32', dataA, [3, 4]);
+
+        // prepare feeds. use model input names as keys.
         // const feeds = { a: tensorA, b: tensorB };
 
-        // // feed inputs and run
+        // feed inputs and run
         // const results = await session.run(feeds);
 
-        // // read from results
+        // read from results
         // const dataC = results.c.data;
         // document.write(`data of result tensor 'c': ${dataC}`);
-        document.getElementById('output_text').innerHTML = 'Success!';
     } catch (e) {
-        document.getElementById('output_text').innerHTML = `failed to inference ONNX model: ${e}.`;
+        document.getElementById('output_text').innerHTML += `failed to perform inference: ${e}.`;
     }
 }
 
