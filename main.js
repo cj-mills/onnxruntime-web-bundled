6e292b34d5d8c5946a0fdab2292eb21ac1dbcda9
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-08-24 12:06:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -56,11 +56,12 @@ async function main() {
 
     document.getElementById('output_text').innerHTML += `<br>${(await return_msg).toString()}`;
 
-    var canvas = document.createElement('canvas');
+    var canvas = document.createElement("CANVAS");
     var context = canvas.getContext('2d');
-    context.drawImage(image, 0, 0);
+    document.body.appendChild(canvas);
     canvas.width = image.width;
     canvas.height = image.height;
+    context.drawImage(image, 0, 0);
     var imageData = context.getImageData(0, 0, image.width, image.height);
 
     // 1. Get buffer data from image.
