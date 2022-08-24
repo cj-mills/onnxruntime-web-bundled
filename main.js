b6135ab074ccdca561adad39e96b14aae774e76f
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-08-24 13:22:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -64,8 +64,8 @@ async function main() {
     var context = canvas.getContext('2d');
     document.body.appendChild(canvas);
     image.onload = () => {
-        canvas.width = image.width;
-        canvas.height = image.height;
+        // canvas.width = image.width;
+        // canvas.height = image.height;
         context.drawImage(image, 0, 0);
     }
 
